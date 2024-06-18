046c8432864627ef242399358dfb4cc65bc64e89
drvzir <dhruvw357@gmail.com>
2024-06-18 23:27:49 +0530

    Office management system

---
+++ pythonProject3/office_emp_proj/emp_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Role , Employee,Department
+
+admin.site.register(Employee)
+admin.site.register(Role)
+admin.site.register(Department)
+
+
+# Register your models here.
